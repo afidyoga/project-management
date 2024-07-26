9c7e4f24d180ac54f462ec48782bad7083b1de1e
afidyoga <afidyoga45dr@gmail.com>
2024-07-26 14:05:57 +0700

    first commit

---
+++ lumen_jwt_rest_api/app/Models/User.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Laravel\Lumen\Auth\Authorizable;
+
+
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class User extends Model implements AuthenticatableContract, AuthorizableContract, JWTSubject
+{
+    use Authenticatable, Authorizable;
+
+    protected $fillable = [
+        'name', 'email'
+    ];
+    protected $hidden = [
+        'password',
+    ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    public function checkUser($email)
+    {
+        try {
+            $result = $this->where("email", $email)->first();
+            if ($result) {
+                return true;
+            }
+            return false;
+        } catch (QueryException $ex) {
+            Log::info("UserModel Error", ["register" => $ex->getMessage(), "line" => $ex->getLine()]);
+            return false;
+        }
+    }
+
+    public function register($name, $email, $password)
+    {
+        try {
+            $this->name = $name;
+            $this->email = $email;
+            $this->password = $password;
+            if ($this->save()) {
+                return true;
+            }
+            return false;
+        } catch (QueryException $ex) {
+            Log::info("UserModel Error", ["register" => $ex->getMessage(), "line" => $ex->getLine()]);
+            return false;
+        }
+    }
+
+    public function getSingleUser($email)
+    {
+        try {
+            $user_email = $this->where('email', $email)->first();
+            if ($user_email) {
+                return $user_email;
+            }
+            return null;
+        } catch (QueryException $ex) {
+            Log::info('UserModel Error', ['getSingleUser' => $ex->getMessage(), 'line' => $ex->getLine()]);
+            return null;
+        }
+    }
+
+    public function getProfile($email)
+    {
+        try {
+            $result = $this->where('email', $email)->first();
+            if ($result) {
+                return $result;
+            }
+            return null;
+        } catch (QueryException $ex) {
+            Log::info('UserModel Error', ['getProfile' => $ex->getMessage(), 'line' => $ex->getLine()]);
+            return null;
+        }
+    }
+
+    public function updatePassword($email, $hashPassword)
+    {
+        try {
+            $result = $this->where('email', $email)
+                ->update(['password' => $hashPassword]);
+            if ($result) {
+                return true;
+            }
+            return false;
+        } catch (QueryException $ex) {
+            Log::info('UserModel Error', ['resetPassword' => $ex->getMessage(), 'line' => $ex->getLine()]);
+            return false;
+        }
+    }
+
+    public function updateSetting($email,$setting_name,$setting_value)
+    {
+        try {
+            $query = $this->newQuery();
+            $query->where('email',$email);
+            if ($setting_name == "email_follow_me"){
+                $query->update(['email_follow_me' => $setting_value]);
+            }
+            elseif ($setting_name == "email_answers_my_post"){
+                $query->update(['email_answers_my_post' => $setting_value]);
+            }
+            elseif ($setting_name == "email_someone_mentions_me"){
+                $query->update(['email_someone_mentions_me' => $setting_value]);
+            }
+            elseif ($setting_name == "new_launches_projects"){
+                $query->update(['new_launches_projects' => $setting_value]);
+            }
+            elseif ($setting_name == "monthly_product_updates"){
+                $query->update(['monthly_product_updates' => $setting_value]);
+            }
+            elseif ($setting_name == "subscribe_to_newsletter"){
+                $query->update(['subscribe_to_newsletter' => $setting_value]);
+            }
+            else{
+                return false;
+            }
+            if ($query){
+                return true;
+            }
+            return false;
+        } catch (QueryException $ex) {
+            Log::info('UserModel Error', ['updateSetting' => $ex->getMessage(), 'line' => $ex->getLine()]);
+            return false;
+        }
+    }
+
+    public function updateProfileHeadline($email,$name,$headline)
+    {
+        try {
+            $result = $this->where('email', $email)
+                ->update(['name' => $name, 'title' => $headline]);
+            if ($result) {
+                return true;
+            }
+            return false;
+        } catch (QueryException $ex) {
+            Log::info('UserModel Error', ['updateProfileHeadline' => $ex->getMessage(), 'line' => $ex->getLine()]);
+            return false;
+        }
+    }
+
+    public function updateProfileInformation($email,$name,$mobile, $description, $location)
+    {
+        try {
+            $result = $this->where('email', $email)
+                ->update(['name' => $name, 'mobile' => $mobile, 'description' => $description, 'location' => $location]);
+            if ($result) {
+                return true;
+            }
+            return false;
+        } catch (QueryException $ex) {
+            Log::info('UserModel Error', ['updateProfileInformation' => $ex->getMessage(), 'line' => $ex->getLine()]);
+            return false;
+        }
+    }
+
+
+
+
+}
